966a70001d078f9954a5b720e6d5d45cf0aa1f8e
RobTov <tovelrob25@gmail.com>
2025-06-03 16:14:08 -0400

    template frontend

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((mod) => mod.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (mod) => mod.LoginComponent
+      ),
+  },
+  {
+    path: 'appointment',
+    loadComponent: () =>
+      import('./components/appointment/appointment.component').then(
+        (mod) => mod.AppointmentComponent
+      ),
+  },
+  {
+    path: 'appointments',
+    loadComponent: () =>
+      import('./pages/appointments/appointments.component').then(
+        (mod) => mod.AppointmentsComponent
+      ),
+  },
+];
---
+++ web/src/app/pages/appointments/appointments.component.ts
@@ -0,0 +1,73 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointments',
+  imports: [],
+  templateUrl: './appointments.component.html',
+  styleUrl: './appointments.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppointmentsComponent {
+  public fakeAppointments = [
+    {
+      name: 'Mariana López',
+      email: 'mariana.lopez@email.com',
+      phone: '55991213',
+      status: 'active',
+    },
+    {
+      name: 'Valeria Gómez',
+      email: 'valeria.gomez@email.com',
+      phone: '58215422',
+      status: 'active',
+    },
+    {
+      name: 'Camila Rodríguez',
+      email: 'camila.rodriguez@email.com',
+      phone: '51179877',
+      status: 'active',
+    },
+    {
+      name: 'Sofía Fernández',
+      phone: '55190023',
+      email: 'sofia.fernandez@email.com',
+      status: 'inactive',
+    },
+    {
+      name: 'Isabella Ramírez',
+      email: 'isabella.ramirez@email.com',
+      phone: '53905005',
+      status: 'active',
+    },
+    {
+      name: 'Gabriela Torres',
+      email: 'gabriela.torres@email.com',
+      phone: '58990167',
+      status: 'inactive',
+    },
+    {
+      name: 'Luciana Vargas',
+      email: 'luciana.vargas@email.com',
+      phone: '54228899',
+      status: 'inactive',
+    },
+    {
+      name: 'Daniela Castillo',
+      email: 'daniela.castillo@email.com',
+      phone: '52152963',
+      status: 'active',
+    },
+    {
+      name: 'Alejandra Pérez',
+      email: 'alejandra.perez@email.com',
+      phone: '55059128',
+      status: 'active',
+    },
+    {
+      name: 'Natalia Sánchez',
+      email: 'natalia.sanchez@email.com',
+      phone: '59993367',
+      status: 'inactive',
+    },
+  ];
+}
