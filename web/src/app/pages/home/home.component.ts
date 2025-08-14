09edc5aa79bf84e8abb496da60f9344ef74b9910
RobTov <tovelrob25@gmail.com>
2025-08-13 22:00:19 -0400

    feat: added clients to the home page

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ServicesModel } from '../../models/services.model';
 import { ServicesResource } from '../../services/api/services.resource';
+import { ClientsModel } from '../../models/clients.model';
+import { ClientsResource } from '../../services/api/clients.resource';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +14,22 @@ import { ServicesResource } from '../../services/api/services.resource';
 export class HomeComponent {
   public isLoading: boolean = true;
   public services: ServicesModel[] = [];
+  public clients: ClientsModel[] = [];
 
   private servicesResource = inject(ServicesResource);
+  private clientsResource = inject(ClientsResource);
 
   ngOnInit(): void {
     this.servicesResource.get();
-    this.servicesResource.services.subscribe(services => {
+    this.servicesResource.services.subscribe((services) => {
       this.services = services;
-    })
-    
+    });
+
+    this.clientsResource.get();
+    this.clientsResource.clients.subscribe((clients) => {
+      this.clients = clients;
+    });
+
     this.isLoading = false;
   }
- }  
+}
