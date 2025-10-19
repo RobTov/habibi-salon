5240bcd81bab5784a0e43b576ce8b9cd0cd81869
RobTov <tovelrob25@gmail.com>
2025-10-19 10:47:42 -0400

    feat: finished the user registration

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -3,6 +3,10 @@ import { LoginService } from '../../services/api/login.service';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import {
+  FakeUserRegistrationModel,
+  RegisterService,
+} from '../../services/api/register.service';
 @Component({
   selector: 'app-login',
   imports: [RouterModule, FormsModule],
@@ -12,12 +16,34 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginComponent {
   public loginService = inject(LoginService);
+  private usersService = inject(RegisterService);
   private router = inject(Router);
-  private toastr = inject(ToastrService)
+  private toastr = inject(ToastrService);
+
+  private users: FakeUserRegistrationModel[] = [];
+
+  ngOnInit(): void {
+    this.users = this.usersService.getUsers();
+  }
 
   public email: string = '';
+  public password: string = '';
 
   public login(): void {
+    const currentUser = this.users.filter((u) => {
+      return u.email === this.email;
+    })[0];
+
+    if (!currentUser) {
+      this.toastr.error('Email Incorrecto', 'Error');
+      return;
+    }
+
+    if (currentUser.password !== this.password) {
+      this.toastr.error('Contraseña Incorrecta', 'Error');
+      return;
+    }
+
     if (this.email.includes('admin')) {
       this.loginService.setRole('admin');
     } else {
@@ -27,6 +53,6 @@ export class LoginComponent {
     this.loginService.setLogged(true);
 
     this.router.navigate(['/home']);
-    this.toastr.success('Inicio de sesión correcto', 'Bienvenido!')
+    this.toastr.success('Inicio de sesión correcto', 'Bienvenido!');
   }
 }
